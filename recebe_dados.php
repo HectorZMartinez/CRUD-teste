ddeeafa20a47a7069a5a65623a132051b87d0447
Hector Zito Martinez <hectormartinez@toledoprudente.edu.br>
2023-06-09 21:32:52 -0300

    Atualização do recebe_dados
    Corrigi um erro no back-end que ocorria quando um usuário enviava um formulário com campos em branco. Agora, a verificação dos campos é feita corretamente, e a página de cadastro do usuário é atualizada se algum campo estiver incorreto ou em branco.
    
    - Utilizei o operador ternário para verificar se os campos estão definidos no $_POST antes de atribuir seus valores às variáveis $nome, $email, $senha e $confirmar_senha. Com isso, evitei erros de chave indefinida.
    - Adicionei uma verificação de conexão com o banco de dados usando $conn->connect_error. Caso ocorra algum erro na conexão, exibimos uma mensagem de falha.
    - Incluí um die() para interromper a execução do script em caso de erro na conexão com o banco de dados.
    - Modifiquei a verificação de registros duplicados no banco de dados para verificar se a variável $result existe antes de verificar o número de linhas retornadas.
    - Adicionei um bloco else no final para lidar com erros de inserção no banco de dados, exibindo a mensagem de erro específica retornada por $conn->error.

--- recebe_dados.php
+++ recebe_dados.php
@@ -2,16 +2,12 @@
 
 include("conecta.php");
 
-$nome = $_POST["nome"];
-
-$email = $_POST["email"];
-
-$senha = $_POST["senha"];
-
-$confirmar_senha = $_POST["confirmaSenha"];
+$nome = isset($_POST["nome"]) ? $_POST["nome"] : "";
+$email = isset($_POST["email"]) ? $_POST["email"] : "";
+$senha = isset($_POST["senha"]) ? $_POST["senha"] : "";
+$confirmar_senha = isset($_POST["confirmaSenha"]) ? $_POST["confirmaSenha"] : "";
 
 $erro = [];
-
 
 function validarCadastro(string $nome, string $email, string $senha, string $confirmar_senha, &$erro)
 {
@@ -21,29 +17,30 @@ function validarCadastro(string $nome, string $email, string $senha, string $con
         $erro["nomeError"] = "O nome informado não é válido.";
     } elseif (strlen($nome) < 3 || strlen($nome) > 50) {
         $Validar = false;
-        $erro["nomeError"] = "O nome informado não é válido EX: deve conter no min 3 e Max de 50 caracteres.\n";
+        $erro["nomeError"] = "O nome informado não é válido. Deve conter no mínimo 3 e no máximo 50 caracteres.";
     }
 
     if (strlen($email) == 0) {
         $Validar = false;
-        $erro["emailError"] = "O e-mail informado não é válido. \n";
+        $erro["emailError"] = "O e-mail informado não é válido.";
     } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $Validar = false;
-        $erro["emailError"] = "O e-mail informado não é válido.\n";
+        $erro["emailError"] = "O e-mail informado não é válido.";
     }
 
     if (strlen($senha) == 0) {
         $Validar = false;
-        $erro["senhaError"] = "A senha informada não é válida.\n";
+        $erro["senhaError"] = "A senha informada não é válida.";
     } elseif (!preg_match("/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/", $senha)) {
         $Validar = false;
-        $erro["senhaError"] = "A senha informada não é válida EX: Min 8 dígitos contendo letras maiúsculas e minúsculas, números, caracteres especiais e não conter sequências. \n";
+        $erro["senhaError"] = "A senha informada não é válida. Deve conter no mínimo 8 caracteres, incluindo letras maiúsculas e minúsculas, números e caracteres especiais.";
     }
 
     if ($senha != $confirmar_senha) {
         $Validar = false;
-        $erro["confirmaError"] = "Os campos preenchidos são válidos.\n";
+        $erro["confirmaError"] = "Os campos de senha não correspondem.";
     }
+
     return $Validar;
 }
 
@@ -51,33 +48,39 @@ if (!validarCadastro($nome, $email, $senha, $confirmar_senha, $erro)) {
     $erro["nome"] = $nome;
     $erro["email"] = $email;
     $arr = json_encode($erro);
-    header("Location:index.php?errors=$arr");
+    header("Location: index.php?errors=$arr");
     exit;
 }
 
 $conn = new mysqli($host . ':' . $port, $user, $password, $db);
 
-$sql = "select * from usuario where email = '$email';";
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM usuario WHERE email = '$email'";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
+if ($result && $result->num_rows > 0) {
     $erro["nome"] = $nome;
     $erro["email"] = $email;
-    $erro["emailError"] =  "Email já cadastrado na base de dados";
+    $erro["emailError"] = "E-mail já cadastrado na base de dados";
     $arr = json_encode($erro);
-    header("Location:index.php?errors=$arr");
+    header("Location: index.php?errors=$arr");
     exit;
 }
 
-$sql = "INSERT INTO usuario (nome, email, senha)
-    VALUES ('$nome', '$email', '$senha')";
-
+$sql = "INSERT INTO usuario (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
 
 if ($conn->query($sql) === TRUE) {
     $ok["nome"] = $nome;
     $ok["email"] = $email;
     $ok["senha"] = $senha;
     $arr = json_encode($ok);
-    header("Location:index.php?ok=$arr");
+    header("Location: index.php?ok=$arr");
     exit;
+} else {
+    echo "Erro ao inserir no banco de dados: " . $conn->error;
 }
+
+$conn->close();
