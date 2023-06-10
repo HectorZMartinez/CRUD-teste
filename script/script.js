b3bbc18c0c854e016bc52dfced79bfd01e451ed4
Hector Zito Martinez <hectormartinez@toledoprudente.edu.br>
2023-06-10 01:29:03 -0300

    Atualização
    Mudança na validação na parte do front que marcava o campo de confirmar senha em verde com os campos de cadastro vazios.

--- script/script.js
+++ script/script.js
@@ -1,67 +1,51 @@
 function validarInputs(event) {
     var error = false;
-    var campoNome = document.querySelector("#nome")
-    var campoEmail = document.querySelector("#email")
-    var campoSenha = document.querySelector("#senha")
-    var campoConfirmaSenha = document.querySelector("#confirmaSenha")
+    var campoNome = document.querySelector("#nome");
+    var campoEmail = document.querySelector("#email");
+    var campoSenha = document.querySelector("#senha");
+    var campoConfirmaSenha = document.querySelector("#confirmaSenha");
 
-    if (campoNome.value.length < 3 || campoNome.value.length > 50) {
-        document.querySelector("#erroNome").style.display = "flex"
-
-        campoNome.style.border = "1px solid red"
+    if (campoNome.value.length < 3 || campoNome.value.length > 50 || campoNome.value.trim() === "") {
+        document.querySelector("#erroNome").style.display = "flex";
+        campoNome.style.border = "1px solid red";
         error = true;
     } else {
-        document.querySelector("#erroNome").style.display = "none"
-
-        campoNome.style.border = "1px solid green"
-
+        document.querySelector("#erroNome").style.display = "none";
+        campoNome.style.border = "1px solid green";
     }
 
-    if (!campoEmail.checkValidity()) {
-        document.querySelector("#erroEmail").style.display = "flex"
-
-        campoEmail.style.border = "1px solid red"
+    if (!campoEmail.checkValidity() || campoEmail.value.trim() === "") {
+        document.querySelector("#erroEmail").style.display = "flex";
+        campoEmail.style.border = "1px solid red";
         error = true;
     } else {
-
-        document.querySelector("#erroEmail").style.display = "none"
-
-        campoEmail.style.border = "1px solid green"
-
+        document.querySelector("#erroEmail").style.display = "none";
+        campoEmail.style.border = "1px solid green";
     }
 
-    if (!campoSenha.checkValidity()) {
-
-        document.querySelector("#erroSenha").style.display = "flex"
-
-        campoSenha.style.border = "1px solid red"
+    if (campoSenha.value.trim() === "") {
+        document.querySelector("#erroSenha").style.display = "flex";
+        campoSenha.style.border = "1px solid red";
+        error = true;
+    } else if (!campoSenha.checkValidity()) {
+        document.querySelector("#erroSenha").style.display = "flex";
+        campoSenha.style.border = "1px solid red";
         error = true;
     } else {
-
-        document.querySelector("#erroSenha").style.display = "none"
-
-        campoSenha.style.border = "1px solid green"
-
-
+        document.querySelector("#erroSenha").style.display = "none";
+        campoSenha.style.border = "1px solid green";
     }
 
-    if (campoConfirmaSenha.value !== campoSenha.value) {
-
-        document.querySelector("#erroConfirmaSenha").style.display = "flex"
-
-        campoConfirmaSenha.style.border = "1px solid red"
+    if (campoConfirmaSenha.value !== campoSenha.value || campoConfirmaSenha.value.trim() === "") {
+        document.querySelector("#erroConfirmaSenha").style.display = "flex";
+        campoConfirmaSenha.style.border = "1px solid red";
         error = true;
     } else {
-
-        document.querySelector("#erroConfirmaSenha").style.display = "none"
-
-        campoConfirmaSenha.style.border = "1px solid green"
-
-
+        document.querySelector("#erroConfirmaSenha").style.display = "none";
+        campoConfirmaSenha.style.border = "1px solid green";
     }
 
     if (error) {
-        //        event.preventDefault()
+        event.preventDefault();
     }
-
 }
